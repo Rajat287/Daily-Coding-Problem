06a634380b3c5de759141dc08569259d42fe701a
Rajat287 <rajat.yadav@phenompeople.com>
2022-06-07 17:56:41 +0530

    First

--- src/main/java/org/example/ProductExceptItself.java
+++ src/main/java/org/example/ProductExceptItself.java
@@ -1,5 +1,5 @@
 package org.example;
-//Given an array of integers, retuen a new array such that each element at
+//Given an array of integers, return a new array such that each element at
 //index i of the new array is the product of all the numbers in the
 //original array except the one at i.
 //example: input- [1,2,3,4,5]   output- [120,60,40,30,24]
