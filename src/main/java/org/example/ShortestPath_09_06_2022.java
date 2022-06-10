51eca9ad823fa3f106c0198b2f17e0689c1216e4
Rajat287 <rajat.yadav@phenompeople.com>
2022-06-10 16:05:06 +0530

    First

--- src/main/java/org/example/ShortestPath_09_06_2022.java
+++ src/main/java/org/example/ShortestPath_09_06_2022.java
@@ -1,5 +1,18 @@
 package org.example;
-/**/
+/*You are given an M by N matrix consisting of booleans that represents a board.
+Each True boolean represents a wall. Each False boolean represents a tile you can walk on.
+Given this matrix, a start coordinate, and an end coordinate,
+return the minimum number of steps required to reach the end coordinate from the start.
+If there is no possible path, then return null. You can move up, left, down, and right.
+You cannot move through walls. You cannot wrap around the edges of the board.
+For example, given the following board:
+
+[[f, f, f, f],
+[t, t, f, t],
+[f, f, f, f],
+[f, f, f, f]]
+and start = (3, 0) (bottom left) and end = (0, 0) (top left),
+the minimum number of steps required to reach the end is 7, since we would need to go through (1, 2) because there is a wall everywhere else on the second row.*/
 public class ShortestPath_09_06_2022 {
     boolean isSafe(char[][] mat, boolean[][] visited, int x, int y){
         return (x>=0&&x<mat.length&&y>=0&&y<mat[0].length&&!visited[x][y]&&mat[x][y]=='f');
