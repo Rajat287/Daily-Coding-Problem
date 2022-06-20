05d53d3032bd05bf4389d455311bb838d0c6e280
Rajat287 <105740032+Rajat287@users.noreply.github.com>
2022-06-20 15:28:45 +0530

    Update Minimum_Insert_20_06_2022.java

--- src/main/java/org/example/Minimum_Insert_20_06_2022.java
+++ src/main/java/org/example/Minimum_Insert_20_06_2022.java
@@ -1,5 +1,9 @@
 package org.example;
+/*Given a string s. In one step you can insert any character at any index of the string.
 
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.*/
 public class Minimum_Insert_20_06_2022 {
 
 
